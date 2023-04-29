54bd7a9318bafd580edf6d6277dd837440885eba
Matheus Galbero Zanetoni <matheus_zanetoni@outlook.com.br>
2023-04-29 18:26:21 -0300

    Pequenas modificações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatInputModule} from '@angular/material/input';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDividerModule} from '@angular/material/divider';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,6 +12,7 @@ import { CustomerComponent } from './customer/customer.component';
 import { CustomerService } from './customer.service';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NgToastModule } from 'ng-angular-popup';
 
 @NgModule({
   declarations: [
@@ -30,7 +30,8 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgToastModule
   ],
   providers: [
     CustomerService
--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CustomerService } from 'src/app/customer.service';
 import { Customer } from './customer';
 import { formatDate } from '@angular/common';
+import { NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-customer',
@@ -9,22 +10,24 @@ import { formatDate } from '@angular/common';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent {
-  errors!: String[];
   customer!: Customer;
-  success: boolean = false;
 
-  constructor(private service: CustomerService) {
+  constructor(private service: CustomerService, private toast: NgToastService) {
     this.customer = new Customer();
   }
 
   onSubmit() {
-    this.customer.birthdateCustomer = formatDate(this.customer.birthdateCustomer, "dd/MM/yyy", "en-US");
+    try {
+      this.customer.birthdateCustomer = formatDate(this.customer.birthdateCustomer, "dd/MM/yyy", "en-US");
+    } catch {
+      this.toast.error({detail: "Erro", summary: `Erro ao cadastrar cliente!`});
+    }
     this.service.save(this.customer).subscribe(response => {
-      this.errors = [];
-      this.success = true;
+      this.customer = new Customer();
+      this.toast.success({detail: "Sucesso", summary: "Cliente cadastrado com sucesso!"});
     }, errorResponse => {
-      this.errors = errorResponse.error.errors;
-      this.success = false;
+      this.customer.birthdateCustomer = formatDate(this.customer.birthdateCustomer, "yyyy-MM-dd", "en-US");
+      this.toast.error({detail: "Erro", summary: `Erro ao cadastrar cliente!`});
     });
   }
 }
